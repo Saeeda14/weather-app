9f0dd7f43767edf1b067e2a847e1fbce7295af24
Saeeda <saeedabatool39@gmail.com>
2023-09-20 21:53:37 -0400

    added the fetch 5-day forecast func

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,30 @@ const apiKey = '022bba9572364968227b2b2289462619';
 
 // func to fetch current weather 
 function fetchCurrentWeather(city) {
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={022bba9572364968227b2b2289462619}';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={022bba9572364968227b2b2289462619}`;
 
     // fetch API to make a GET request 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
+            // current weather displayed here 
             console.log(data);
         })
         .catch(error => console.error('Error fetching current weather:', error));
 }
 
+// func to fetch 5-day weather forecast 
+function fetchForecast(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={022bba9572364968227b2b2289462619}`;
+
+    // fetch API to make a GET request
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // 5-day forecast displayed here 
+            console.log(data);
+        })
+        .catch(error => console.error('Error fetching forecast:', error));
+}
+
+// 
